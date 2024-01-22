7b569f704bc3f32590763854e0f46ec5b8ab218a
saquibhamza333 <saquibhamza333@gmail.com>
2024-01-22 23:27:33 +0530

    added forms for adding hotels

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth'
 import cookieParser from 'cookie-parser'
 import path from 'path';
 import { v2 as cloudinary } from 'cloudinary';
+import myHotelRoutes from './routes/my-hotels'
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -26,6 +27,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use('/api/auth',authRoutes) ;
 app.use("/api/users",userRoutes);
+app.use("/api/my-hotels",myHotelRoutes);
 
 app.listen(7000,()=>{
     console.log("server listening on localhost:7000")
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter as Router,Route,Routes, Navigate } from 'react-router-dom
 import Layout from './layouts/Layout'
 import Register from './pages/Register'
 import  SignIn  from './pages/SignIn'
+import AddHotel from './pages/AddHotel'
+import { useAppContext } from './contexts/AppContext'
+
 
 const  App=()=> {
-  
+  const {isLoggedIn} = useAppContext();
 
   return (
     <Router>
@@ -40,6 +43,15 @@ const  App=()=> {
               </Layout>
           }
               />
+              {isLoggedIn && <>
+              <Route path ='/add-hotel' element= {
+                <Layout>
+                  <AddHotel/>
+                </Layout>
+                
+              } />
+              
+              </>}
 
 
           <Route path ='*' element= {<Navigate to= "/" />} />
